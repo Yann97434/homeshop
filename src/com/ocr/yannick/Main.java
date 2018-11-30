a8ad48f8280bf2635c0863d6c19be4e03d014530
Yannick NILLAMA-IRESSOU <yannick.nillama@gmail.com>
2018-11-30 17:56:08 -0300

    -add new exception NoProductInBillException -catch the exception in the main

--- src/com/ocr/yannick/Main.java
+++ src/com/ocr/yannick/Main.java
@@ -10,10 +10,30 @@ public class Main {
         Customer customer = new Customer("Juste Leblanc", "19 rue Germain Pilon, Paris");
 
         Bill bill = new Bill(customer, new RelayDelivery(27));
-        bill.addProduct(cafe, 1);
+        /*bill.addProduct(cafe, 1);
         bill.addProduct(tv, 1);
-        bill.addProduct(fridge, 1);
+        bill.addProduct(fridge, 1); */
 
-        bill.generate(new FileWriter("facture_leblanc"));
+
+        try {
+            bill.generate(new Writer() {
+                @Override
+                public void start() {
+
+                }
+
+                @Override
+                public void writeLine(String line) {
+                    System.out.println(line);
+                }
+
+                @Override
+                public void stop() {
+
+                }
+            });
+        } catch (NoProductInBillException e) {
+            System.out.println("Pas de produit dans la facture");
+        }
     }
 }
